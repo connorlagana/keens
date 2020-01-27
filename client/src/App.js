36342997ad43546e2fdd289594ee78534a18a49c
henrydozie <hcewurum@gmail.com>
2020-01-27 15:04:16 -0500

    HC made a slider component, images and added soma animation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Hero from './components/Hero'
+import Header from './components/Header'
 
 import Location from "./components/Location.js";
 import Menu from "./components/Menu.js";
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         {/* <h1>Hello World</h1> */}
         <Hero/>
         <Menu />
---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo">
+          <img src="https://imgur.com/CbhUXUB.png" alt="logo" className="logoImage"/>
+        </div>
+        <div className="nav">
+          <Link to="#"> Reservations</Link>
+          <Link to="#">About</Link>
+          <Link to="#">Menus</Link>
+          <Link to="#">Private Events</Link>
+          <Link to="#">Galley</Link>
+          <Link to="#">Contacts</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
