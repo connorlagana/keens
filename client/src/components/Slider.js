247e0dca1ac5f7463da9e23d0a2ea36a4b59a8a4
henrydozie <hcewurum@gmail.com>
2020-01-27 17:08:41 -0500

    finished the carousel

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,7 +1,8 @@
 import React from "react";
 import AwesomeSlider from "react-awesome-slider";
-import { styles } from "react-awesome-slider/dist/styles.css";
-
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+// import AwesomeSliderStyles from "react-awesome-slider/src/styled/fold-out-animation/fold-out-animation.scss";
+import AwesomeSliderStyles from "react-awesome-slider/dist/styles.css";
 
 
 import image_1 from "../images/image4.png";
@@ -9,18 +10,26 @@ import image_2 from "../images/image1.jpg";
 import image_3 from "../images/image2.jpg";
 import image_4 from "../images/image3.jpg";
 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
 const Slider = () => {
 
   return (
     <div className="slider">
-      <AwesomeSlider cssModule={styles}>
+      <AutoplaySlider
+        play={true}
+        cancelOnInteraction={false}
+        interval={3000}
+        animation="cubeAnimation"
+        cssModule={AwesomeSliderStyles}
+      >
         <div data-src={image_1} />
         <div data-src={image_2} />
         <div data-src={image_3} />
         <div data-src={image_4} />
         {/* <div data-src="/path/to/image-1.png" />
     <div data-src="/path/to/image-2.jpg" /> */}
-      </AwesomeSlider>
+      </AutoplaySlider>
     </div>
   );
 };
