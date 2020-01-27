f57977f9e7adcde680dfe296a9edd8068aaca733
Connor Lagana <connor.lagana@gmail.com>
2020-01-27 10:21:52 -0500

    menu

---
+++ client/src/components/Location.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Location extends Component {
+  render() {
+    return (
+      <div className="Location">
+        <h1>Location</h1>
+      </div>
+    );
+  }
+}
+
+export default Location;
