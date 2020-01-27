bcfff1df00d5496f69d978114cef1923d56f9f27
henrydozie <hcewurum@gmail.com>
2020-01-27 12:17:56 -0500

    updated everything so far

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Hero from './components/Hero'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        {/* <h1>Hello World</h1> */}
+        <Hero/>
       </div>
     );
   }
